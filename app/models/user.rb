bbf2b34eabec207ada196cd83f93dd3f5f19a50d
EC2 Default User <ec2-user@ip-172-31-15-227.ap-northeast-1.compute.internal>
2024-02-08 09:13:33 +0000

    [Add] 閲覧数一覧

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  
+  has_many :read_counts, dependent: :destroy
 
   
   has_one_attached :profile_image
