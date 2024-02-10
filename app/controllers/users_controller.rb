b59c13f030d4517bd95e619ca048af320f972ef1
EC2 Default User <takizawasayuki0912@gmail.com>
2024-02-10 14:03:43 +0000

    [Add] 投稿数の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,10 @@ class UsersController < ApplicationController
         @entry = Entry.new
       end
     end
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
